44757e02208162e84185ee271d7d1c4369355d91
MinBeom-Kim <kmb3755@gmail.com>
2020-05-13 16:06:04 +0900

    init

---
+++ build/CMakeFiles/videocapture_basic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/videocapture_basic.dir/videocapture_basic.cpp.o"
+  "videocapture_basic.pdb"
+  "videocapture_basic"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/videocapture_basic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
