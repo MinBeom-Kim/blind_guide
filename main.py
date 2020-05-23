46b63c1b01ed927581f033e9b5b6e9936afdc2dc
MinBeom-Kim <kmb3755@gmail.com>
2020-05-24 02:34:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ while(cap.isOpened()):
     
     kernel = np.ones((16, 16), np.uint8)
 
-    ret, frame = cap.read()
+    ret, cap_frame = cap.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.GaussianBlur(frame, (7, 7), 1.5)
-    frame = cv2.Canny(frame, 50, 100)
-    cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (16, 16))
-    cv2.morphologyEx(frame, cv2.MORPH_CLOSE, (16, 16))
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #cv2.GaussianBlur(frame, (7, 7), 1.5)
+    frame = cv2.Canny(cap_frame, 50, 100)
+    #cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (16, 16))
+    #cv2.morphologyEx(frame, cv2.MORPH_CLOSE, (16, 16))
 
     ret,frame_result1 = cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY)
 
@@ -22,7 +22,8 @@ while(cap.isOpened()):
     frame_blur = cv2.GaussianBlur(frame, (5,5), 0)
     ret, frame_result3 = cv2.threshold(frame_blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-    cv2.imshow("SOURCE", frame)
+
+    cv2.imshow("SOURCE", cap_frame)
     cv2.imshow("THRESH_BINARY", frame_result1)
     cv2.imshow("THRESH_OTSU", frame_result2)
     cv2.imshow("THRESH_OTSU + Gaussian filtering", frame_result3)
