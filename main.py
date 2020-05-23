af88e79b26b3857bf476608dfa32f3935df72e2f
MinBeom-Kim <kmb3755@gmail.com>
2020-05-24 01:49:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,25 +7,25 @@ while(cap.isOpened()):
     
     kernel = np.ones((16, 16), np.uint8)
 
-    ret, cap = cap.read()
+    ret, frame = cap.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.GaussianBlur(cap, (7, 7), 1.5)
-    cap = cv2.Canny(cap, 50, 100)
+    cv2.GaussianBlur(frame, (7, 7), 1.5)
+    cap = cv2.Canny(frame, 50, 100)
     cv2.getStructuringElement(cv2.MORPH_ELLIPSE, kernel)
-    cv2.morphologyEx(cap, cv2.MORPH_CLOSE, kernel)
+    cv2.morphologyEx(frame, cv2.MORPH_CLOSE, kernel)
 
-    ret,cap_result1 = cv2.threshold(cap, 127, 255, cv2.THRESH_BINARY)
+    ret,frame_result1 = cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY)
 
-    ret, cap_result2 = cv2.threshold(cap, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    ret, frame_result2 = cv2.threshold(frame, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-    cap_blur = cv2.GaussianBlur(cap, (5,5), 0)
-    ret, cap_result3 = cv2.threshold(cap_blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    frame_blur = cv2.GaussianBlur(frame, (5,5), 0)
+    ret, frame_result3 = cv2.threshold(frame_blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-    cv2.imshow("SOURCE", cap)
-    cv2.imshow("THRESH_BINARY", cap_result1)
-    cv2.imshow("THRESH_OTSU", cap_result2)
-    cv2.imshow("THRESH_OTSU + Gaussian filtering", cap_result3)
+    cv2.imshow("SOURCE", frame)
+    cv2.imshow("THRESH_BINARY", frame_result1)
+    cv2.imshow("THRESH_OTSU", frame_result2)
+    cv2.imshow("THRESH_OTSU + Gaussian filtering", frame_result3)
 
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
