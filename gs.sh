ea8cff058871407275ff8d9054a30f46e4cf73f0
MinBeom-Kim <kmb3755@gmail.com>
2020-05-28 15:50:37 +0900

    Update gs.sh

--- gs.sh
+++ gs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gst-launch-1.0 -v tcpclientsrc host=192.168.0.31 port=5000 ! gdpdepay ! rtph264depay ! avdec_h264 ! videoconvert ! autovideosink sync=false
+gst-launch-1.0 -v tcpclientsrc host=113.198.244.122 port=5000 ! gdpdepay ! rtph264depay ! avdec_h264 ! videoconvert ! autovideosink sync=false
