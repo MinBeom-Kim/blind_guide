f03650f21c898c7464980317ce4259de249ea8ed
MinBeom-Kim <kmb3755@gmail.com>
2020-05-30 22:57:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import cv2
 import time
 
 # cap = cv2.VideoCapture('./src/test_04.mp4')
-cap = cv2.VideoCapture('gst-launch-1.0 -v tcpclientsrc host=113.198.244.147 port=5000 ! gdpdepay ! rtph264depay ! avdec_h264 ! videoconvert ! autovideosink sync=false') 
+cap = cv2.VideoCapture('udpsrc port=5000 caps = "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" ! rtph264depay ! decodebin ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
 
 while(cap.isOpened()):
     
