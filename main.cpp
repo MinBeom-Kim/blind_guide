3c92e701dee6f14e2b25e3e0b7691fb3586084e5
MinBeom-Kim <kmb3755@gmail.com>
2020-05-30 22:27:34 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -174,7 +174,7 @@ int main(int argc, char* argv[])
 
    cv::VideoWriter writer;
 
-   writer.open("appsrc !  video/x-raw,  width=640, height=480 ! videoconvert ! h264parse ! rtph264pay ! gdppay ! tcpserversink host=192.168.0.18 port=5000", 0, (double)30, cv::Size(640, 480), false);
+   writer.open("raspivid -t 0 -h 720 -w 1280 -fps 25 -b 2000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=192.168.0.18 port=5000", 0, (double)30, cv::Size(640, 480), false);
 
    cv::Mat frame, grayImage, edgeImage;
 
