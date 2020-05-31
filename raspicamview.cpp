42f54473ffff6db7e3787cec1ed1da9f138c59ce
MinBeom-Kim <kmb3755@gmail.com>
2020-06-01 00:22:03 +0900

    Update raspicamview.cpp

--- raspicamview.cpp
+++ raspicamview.cpp
@@ -27,8 +27,8 @@ int main()
 	//writer.open("appsrc ! video/x-raw, framerate=30/1, width=640, height=480 ! autovideoconvert ! autovideosink sync=false", 0, (double)30, cv::Size(640, 480), true);
     //writer.open("appsrc ! video/x-raw, framerate=30/1, width=640, height=480 ! autovideoconvert ! videoflip method=counterclockwise ! autovideosink sync=false", 0, (double)30, cv::Size(640, 480), true);
 	//writer.open("appsrc ! videoconvert ! x264enc tune=zerolatency ! rtph264pay ! udpsink host=192.168.0.89 port=5000", 0, (double)30, cv::Size(640, 480), false);
-	//writer.open("appsrc ! videoconvert ! x264enc tune=zerolatency ! mpegtsmux ! udpsink host=192.168.0.89 port=5000", 0, (double)30, cv::Size(640, 480), false);
-	writer.open("appsrc !  video/x-raw,  width=640, height=480 ! videoconvert ! x264enc tune=zerolatency ! rtph264pay ! gdppay ! tcpserversink host=192.168.168.101 port=5000", 0, (double)30, cv::Size(640, 480), false);
+	writer.open("appsrc ! videoconvert ! x264enc tune=zerolatency ! mpegtsmux ! udpsink host=192.168.0.5 port=5000", 0, (double)30, cv::Size(640, 480), false);
+	//writer.open("appsrc !  video/x-raw,  width=640, height=480 ! videoconvert ! x264enc tune=zerolatency ! rtph264pay ! gdppay ! tcpserversink host=192.168.168.101 port=5000", 0, (double)30, cv::Size(640, 480), false);
     if (!writer.isOpened()) {
         printf("=ERR= can't create writer\n");
         return -1;
